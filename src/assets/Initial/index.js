1721933ffc231abb23f7db1ce6d9828928b8e25d
Daniel Nascimento Firmiano <daniel_2dfla@hotmail.com>
2022-11-16 09:18:39 -0300

    Atualização

--- src/assets/Initial/index.js
+++ src/assets/Initial/index.js
@@ -5,14 +5,14 @@ import Banner from '../../img/banner.png'
 const index = () => {
   return (
     <Container id="initial">
-        <Information >
-            <h2>Olá , sou <em><strong>Daniel Firmiano</strong></em></h2>
-            <h2>Sou desenvolvedor Front-End</h2>
-            <a href='https://mega.nz/file/22hwjagB#_QaK7ubUzUcnUwR7lzYG2BQFvxjALQkl1XXn4s2xhBA' target="_blank" rel="noopener noreferrer">
-              <button >Curriculo</button>
-            </a>
-        </Information>
-        <img src={Banner} alt='Plano de fundo'  />
+      <Information >
+        <h2>Olá , sou <em><strong>Daniel Firmiano</strong></em></h2>
+        <h2>Sou desenvolvedor Front-End</h2>
+        <a href='https://mega.nz/file/22hwjagB#_QaK7ubUzUcnUwR7lzYG2BQFvxjALQkl1XXn4s2xhBA' target="_blank" rel="noopener noreferrer">
+          <button >Curriculo</button>
+        </a>
+      </Information>
+      <img src={Banner} alt='Plano de fundo' />
     </Container>
   )
 }
