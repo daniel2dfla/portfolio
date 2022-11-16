f46f7c9497aa1b831081ec57a0aa7f4716f160fa
Daniel Nascimento Firmiano <daniel_2dfla@hotmail.com>
2022-11-16 10:15:26 -0300

    att

--- src/assets/About/index.js
+++ src/assets/About/index.js
@@ -10,7 +10,7 @@ const index = () => {
         <img src={Avatar} alt='Meu avatar'  />
         <About>
           <h2>Sobre mim!</h2>
-          <p>Gosto de dizer que sou o protagonista da minha história, corro sempre atrás dos meus objetivos, sempre tenho a ética como um pilar. Aprendo um pouco a cada dia, tendo em mente que amanhã terei mais conhecimento do que hoje, dividir para conquistar não é mesmo? Sou um desenvolvedor Full-Stack estudante da Labenu, amo tecnologia e gosto de trabalhar em equipe. </p>
+          <p>Meu nome é Daniel Nascimento Firmiano, programador Front-End júnior. Gosto de dizer que sou o protagonista da minha história, corro sempre atrás dos meus objetivos, sempre tenho a ética como um pilar. Aprendo um pouco a cada dia, tendo em mente que amanhã terei mais conhecimento do que hoje, dividir para conquistar não é mesmo? Sou um desenvolvedor Full-Stack estudante da Labenu, amo tecnologia e gosto de trabalhar em equipe. </p>
         </About>
         
       </Cont>
